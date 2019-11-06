06ee0b4eda2b351aac12bee569af871a7b127796
zero9527 <zero8297183@163.com>
2019-11-06 15:11:25 +0800

    修改回到顶部按钮定位

--- src/components/scrollToTop/index.tsx
+++ src/components/scrollToTop/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import styles from './scrollToTop.scss';
 
+const { useState, useEffect } = React;
+
 /**
  * scrollToTop
  */
 function scrollToTop() {
+  const [showBtn, setShowBtn] = useState(false);
+
+  useEffect(() => {
+    const height = window.innerHeight;
+
+    // 滚动距离大于一屏高度则显示，否则隐藏
+    setShowBtn(() => (
+      document.body.scrollTop >= height
+      || document.documentElement.scrollTop >= height
+    ));
+  }, [document.body.scrollTop, document.documentElement.scrollTop]);
+
   function toTop() {
     if (window.scroll) {
       window.scroll({ top: 0, left: 0, behavior: 'smooth' });
@@ -16,7 +30,11 @@ function scrollToTop() {
   }
 
   return (
-    <div className={styles['top-btn']} onClick={toTop}>
+    <div 
+      className={styles['top-btn']} 
+      onClick={toTop}
+      style={{visibility: showBtn ? 'visible' : 'hidden'}}
+    >
       <i className="iconfont icon-arrow-upward-outline" />
     </div>
   );
